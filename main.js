3fe83c139e13e5c2ae8c39e27c7a9931e02fe2d5
tightcode <tightcodedev@gmail.com>
2019-05-09 12:55:36 -0700

    0.1.0 initial release

---
+++ main.js
@@ -0,0 +1,26 @@
+/* Preloadr 0.1.0 */
+var loadr = document.querySelector("#loader");
+var loadrImg = document.querySelector("#loader img");
+var wrappr = document.querySelector("#wrapper");
+var preloadImg = new Image();
+preloadImg.src = "400x356.png";
+loadrImg.classList.add("removed");
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.classList.remove("loading");
+  if (document.body.classList.length === 0) {
+    document.body.removeAttribute("class");
+  }
+  wrappr.classList.remove("hidden");
+  if (wrappr.classList.length === 0) {
+    wrappr.removeAttribute("class");
+  }
+});
+preloadImg.onload = function() {
+  loadrImg.classList.remove("removed", "hidden");
+};
+window.onload = function() {
+  loadr.classList.remove("visible");
+  setTimeout(function() {
+    document.body.removeChild(loadr);
+  }, 500);
+};
