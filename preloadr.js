e9576e6989f5cf4968f05b3171d8b372970bece6
tightcode <tightcodedev@gmail.com>
2019-05-10 01:34:30 -0700

    0.2.0 major update

--- preloadr.js
+++ preloadr.js
@@ -1,4 +1,4 @@
-/* Preloadr 0.1.0 */
+/* Preloadr 0.2.0 */
 var loadr = document.querySelector("#loader");
 var loadrImg = document.querySelector("#loader img");
 var wrappr = document.querySelector("#wrapper");
